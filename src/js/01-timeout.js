0ecb69d74a7f28255bb4296e71d4b688252f3fae
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-07-09 16:31:05 +0300

    1-3

--- src/js/01-timeout.js
+++ src/js/01-timeout.js
@@ -4,6 +4,30 @@ import '../css/common.css';
  * Метод window.setTimeout(callback, delay, args)
  */
 
+// const logMessage = () => {
+//     console.log('Лог при  вызове callback функци через 3 секунды');
+// }
+
+// console.log('До вызова setTimeout');
+
+// setTimeout(() => {
+//     console.log('Внутри callback для setTimeout')
+// }, 2000)
+
+// console.log('После вызова setTimeout');
+
 /*
  * Очистка таймаута с clearTimeout(timeoutId)
  */
+
+const logger = time => {
+    console.log(`Лог через ${time}ms, потому что мы не отменили таймаут`);
+}
+
+const timerId = setTimeout(logger, 2000, 2000);
+
+const shouldCancelTimer = Math.random() > 0.3;
+
+if (shouldCancelTimer) {
+    clearTimeout(timerId);
+}
