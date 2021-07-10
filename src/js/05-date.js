d06ff7cb8f4f9635a33ccb148cf5a9f1bd0535f1
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-07-10 19:45:29 +0300

    4-6 -done

--- src/js/05-date.js
+++ src/js/05-date.js
@@ -7,3 +7,15 @@ import '../css/common.css';
  - Разница времени
  - Date.now()
 */
+
+const date1 = new Date().getTime();
+console.log('date1', date1);
+
+setTimeout(() => {
+    const date2 = new Date().getTime();
+
+    console.log('date1', date1);
+    console.log('date2', date2);
+
+    console.log(date2 - date1);
+}, 3000)
